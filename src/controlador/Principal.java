64daca98bd14877033b5867857f9d6dd7dd331d2
Joseph Luca <jurassicjoe03@gmail.com>
2022-04-27 13:35:31 +0200

    Primer commit con main vacío

---
+++ src/controlador/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package controlador;
+
+/**
+ * @author 34660
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
