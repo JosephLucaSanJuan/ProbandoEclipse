4186e3727b342903f875543b30a6d505d920790e
Joseph Luca <jurassicjoe03@gmail.com>
2022-04-27 13:40:48 +0200

    Segundo commit con hola mundo con variable nombre

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -9,12 +9,13 @@ package controlador;
  */
 public class Principal {
 
+	String nombre = "Joseph";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola mundo");
 	}
 
 }
